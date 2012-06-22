91fc8eb72d6af5e15176738c7f9a9ec0a1cdcefe
zeebinz <zeebinz@gmail.com>
2012-06-22 02:56:15 +0100

    Add Config type

---
+++ classes/Generator/Generator/Type/Config.php
@@ -0,0 +1,16 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+/**
+ * Generator Config type.
+ *
+ * @package    Generator 
+ * @category   Generator/Types 
+ * @author     Zeebee 
+ * @copyright  (c) 2012 Zeebee 
+ * @license    BSD revised 
+ */
+class Generator_Generator_Type_Config extends Generator_Type 
+{
+	protected $_template = 'generator/type_config';
+	protected $_folder   = 'config';
+
+} // End Generator_Generator_Type_Config 
---
+++ classes/Generator/Type/Config.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+/**
+ * Transparent extension for Generator Config type.
+ *
+ * @package    Generator 
+ * @category   Generator/Types 
+ * @author     Zeebee 
+ * @copyright  (c) 2012 Zeebee 
+ * @license    BSD revised 
+ */
+class Generator_Type_Config extends Generator_Generator_Type_Config {} 
