7eb42dbb52d4be38de9ea86a1ad85385cc661262
zeebinz <zeebinz@gmail.com>
2012-06-24 12:12:46 +0100

    Add generate:config

---
+++ classes/Task/Generate/Config.php
@@ -0,0 +1,30 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+/**
+ * Generates configuration files, optionally with simple config entries
+ * passed as value definitions.
+ *
+ * Additional options:
+ *
+ *   --name=CONFIG (required)
+ *
+ *     This sets the name of the config file.
+ *
+ *   --values=VALUES
+ *
+ *     Value definitions may be added as a comma-separated list in the
+ *     format: "array.key.path|value".
+ *
+ * Examples
+ * ========
+ * minion generate:config --name=logger --module=logger \
+ *     --values="logger.file.name|log, logger.file.ext|txt, logger.debug|1"
+ *
+ *     file : MODPATH.'/logger/config/logger.php'
+ *
+ * @package    Generator
+ * @category   Tasks
+ * @author     Zeebee
+ * @copyright  (c) 2012 Zeebee
+ * @license    BSD revised
+ */
+class Task_Generate_Config extends Generator_Task_Generate_Config {}
