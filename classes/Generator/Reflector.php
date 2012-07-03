82588e35b97a189b6a86b515eff9dcfea1ad3cb4
zeebinz <zeebinz@gmail.com>
2012-07-03 10:40:49 +0100

    Add Generator_Reflector class

---
+++ classes/Generator/Reflector.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+/**
+ * Transparent extension of Generator_Generator_Reflector.
+ *
+ * @package    Generator
+ * @category   Reflectors
+ * @author     Zeebee
+ * @copyright  (c) 2012 Zeebee
+ * @license    BSD revised
+ */
+class Generator_Reflector extends Generator_Generator_Reflector {}
