3f84c8c3d28135b407539e2ed021170632accb08
zeebinz <zeebinz@gmail.com>
2012-07-05 16:28:56 +0100

    Add doccomment template
    This is all PHP as it's a PITA otherwise.

---
+++ views/generator/type_doccomment.php
@@ -0,0 +1,35 @@
+<?php
+
+// Set the indentation level
+$tabs = (isset($tabs) AND is_int($tabs)) ? str_repeat("\t", $tabs) : '';
+
+// Open doccomment
+echo '/**'.PHP_EOL;
+
+// Short description
+if (isset($short_description)) foreach ((array) $short_description as $line)
+{
+	echo $tabs.' * '.$line.PHP_EOL;
+}
+
+// Long description
+if (isset($long_description))
+{
+	echo $tabs.' *'.PHP_EOL;
+	foreach ((array) $long_description as $line)
+	{
+		echo $tabs.' * '.$line.PHP_EOL;
+	}
+}
+
+// Spacer
+echo $tabs.' *'.PHP_EOL;
+
+// Tags
+if (isset($tags)) foreach ((array) $tags as $tag)
+{
+	echo $tabs.' * '.$tag.PHP_EOL;
+}
+
+// Close doccomment
+echo $tabs.' */';
